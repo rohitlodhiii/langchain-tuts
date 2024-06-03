2e9b9bcf1d091e53bf40bf9508b6166b67fa84a9
rohitlodhiii <rohitlodhi0701@gmail.com>
2024-06-03 18:08:06 +0530

    add google genai and fetch on frontend

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask , render_template , jsonify , request
+from dotenv import load_dotenv
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 app = Flask(__name__)
+
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+
 
 @app.route("/")
 def home():
@@ -8,8 +17,12 @@ def home():
 @app.route("/generate" , methods = ['GET' , 'POST'])
 def generate():
     if request.method == 'POST':
-        output = request.form
-        return jsonify({"a" : "b"})
+        prompt = PromptTemplate.from_template("Write a blog on title {title}")
+        llm = ChatGoogleGenerativeAI(model="gemini-pro", temperature=0.3 ,google_api_key=api_key)
+        chain = prompt | llm 
+        mains = request.json.get('prompt')
+        output = chain.invoke(mains).content
+        return output
 
 
 
