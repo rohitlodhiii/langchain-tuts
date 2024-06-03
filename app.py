6f3516c03bfe9003e88e59c4b585c5121d22aa46
rohitlodhiii <rohitlodhi0701@gmail.com>
2024-06-03 16:48:44 +0530

    add post req

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask , render_template
+from flask import Flask , render_template , jsonify , request
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
     return render_template("index.html")
+
+@app.route("/generate" , methods = ['GET' , 'POST'])
+def generate():
+    if request.method == 'POST':
+        output = request.form
+        return jsonify({"a" : "b"})
+
+
 
 
 app.run(host = '0.0.0.0' , port=81)
